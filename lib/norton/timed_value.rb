77c98ca224307e0610744cef6a19bbf9c300e177
Larry Zhao <thehiddendepth@gmail.com>
2015-08-17 23:15:21 +0800

    add timed_value

---
+++ lib/norton/timed_value.rb
@@ -0,0 +1,40 @@
+module Norton
+  module TimedValue
+    extend ActiveSupport::Concern
+
+    module ClassMethods
+
+      def timed_value(name, options={}, &blk)
+        if options[:ttl].nil?
+          raise 'Time to live not specified.'
+        end
+
+        if blk.nil?
+          raise 'Value generation not provided'
+        end
+
+        # Define getter
+        define_method(name) do
+          Norton.redis.with do |conn|
+            v = conn.get("#{self.class.to_s.tableize}:#{self.id}:#{name}")
+
+            if v.nil?
+              v = instance_eval(&blk)
+              conn.setex("#{self.class.to_s.tableize}:#{self.id}:#{name}", options[:ttl], v)
+            end
+
+            v
+          end
+        end
+
+        define_method("reset_#{name}") do
+          Norton.redis.with do |conn|
+            v = instance_eval(&blk)
+            conn.setex("#{self.class.to_s.tableize}:#{self.id}:#{name}", options[:ttl], v)
+            v
+          end
+        end
+      end
+    end
+  end
+end
