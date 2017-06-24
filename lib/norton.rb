6ecf0a6b50e7060a7f7a09ecdcd05f980c2bc4ad
Larry Zhao <larry@jianshu.com>
2017-06-24 23:05:06 +0800

    ok

--- lib/norton.rb
+++ lib/norton.rb
@@ -31,5 +31,57 @@ module Norton
         Redis.new(options.slice(:url, :host, :port, :db, :path, :password, :namespace, :ssl_params, :driver))
       end
     end
+
+    #
+    # 为多个 Norton 对象一次性取出多个值
+    #
+    # 从一组相同的对象中，一次性取出多个 Norton 值。
+    #
+    # 例如:
+    #
+    # vals = Norton.norton_vals([user1, user2, user3], [followers_count, profile_updated_at])
+    #
+    # 将会返回:
+    #
+    # ```
+    # {
+    #   "users:1:followers_count": 2,
+    #   "users:2:followers_count": 3,
+    #   "users:3:followers_count": 4,
+    #   "users:1:profile_updated_at": 1498315792,
+    #   "users:2:profile_updated_at": 1499315792,
+    #   "users:3:profile_updated_at": 1409315792,
+    # }
+    # ```
+    #
+    # * 返回的 Field 之间的顺序无法保证
+    #
+    # @param [Array] objects
+    # @param [Array] names
+    #
+    # @return [Hash]
+    #
+    def norton_vals(objects, names)
+      ret = {}
+      redis_keys = []
+
+      objects.each do |obj|
+        next if obj.nil?
+
+        names.each do |n|
+          redis_keys << obj.norton_redis_key(n)
+        end
+      end
+
+      values = Norton.redis.with do |conn|
+        conn.mget(redis_keys)
+      end
+
+      redis_keys.each_with_index do |k, i|
+        ret[k] = values[i]
+      end
+
+      ret
+    end
   end
 end
--- spec/norton_spec.rb
+++ spec/norton_spec.rb
@@ -7,4 +7,37 @@ describe Norton do
       expect(Norton.redis).not_to be_nil
     end
   end
+
+  describe "norton_vals" do
+    class Foobar
+      include Norton::Counter
+      include Norton::Timestamp
+
+      counter :test_counter
+      timestamp :test_timestamp
+
+      def initialize(id)
+        @id = id
+      end
+
+      def id
+        @id
+      end
+    end
+
+    it "should get the values correctly" do
+      foobar1 = Foobar.new(SecureRandom.hex(2))
+      foobar2 = Foobar.new(SecureRandom.hex(2))
+
+      Random.rand(100).times { foobar1.incr_test_counter }
+      foobar1.touch_test_timestamp
+
+      sleep(2)
+
+      Random.rand(100).times { foobar2.incr_test_counter }
+      foobar2.touch_test_timestamp
+
+      vals = Norton.norton_vals([foobar1, foobar2], [:test_counter, :test_timestamp])
+    end
+  end
 end
