df567a45319537d87397537e5b3df425b92c1f04
Larry Zhao <larry@jianshu.com>
2017-06-25 16:42:08 +0800

    try again

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,11 @@ require "norton"
 require "rspec"
 require "timecop"
 
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  CodeClimate::TestReporter::Formatter
+]
+
 if ENV['CIRCLE_ARTIFACTS']
   dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
   SimpleCov.coverage_dir(dir)
