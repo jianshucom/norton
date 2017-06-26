b681a970bbfd928a7f7a9b560b02f8de359c4c1e
Larry Zhao <larry@jianshu.com>
2017-06-26 21:33:14 +0800

    fix codeclimate code coverage settings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,15 +6,12 @@ require "norton"
 require "rspec"
 require "timecop"
 
+CodeClimate::TestReporter.start
+
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   SimpleCov::Formatter::HTMLFormatter,
   CodeClimate::TestReporter::Formatter
 ]
-
-if ENV['CIRCLE_ARTIFACTS']
-  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
-  SimpleCov.coverage_dir(dir)
-end
 
 SimpleCov.start
 
